0307015bf03a6f08ec02d1f9a26dbf0f884110c3
JORDAN TUCK [Student] <jordan.tuck@sl.on.ca>
2019-10-07 16:24:55 -0400

    gun fire control done, no animations

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Gun
+{
+    override protected void Update()
+    {
+        base.Update();                              
+        if ((Input.GetMouseButtonDown(0)) && ((Time.time - lastFireTime) > fireRate))     //Shotgun and Pistol are semi-auto fire
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
